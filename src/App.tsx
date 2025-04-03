78f85f30668b7c2e1946d8f36b75a9d42eb6016e
SPerrierPro <sophie.perrier.pro@gmail.com>
2025-04-03 11:55:08 +0200

    Started the project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import data from './data/data.json'
+import './App.css'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+
+function App() {
+
+
+  return (
+    <body>
+      <NavBar />
+
+      <Footer />
+    </body>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,10 @@
+function NavBar() {
+    return (
+        <nav>
+            <img src="/src/assets/images/swiftplay-sports-text.png" alt="SwiftPlay Sports logo" />
+            <h1>SwiftPlay Sports</h1>
+        </nav>
+    )
+}
+
+export default NavBar;
